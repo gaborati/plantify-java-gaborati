115231dcdf607580c90fb644b60a6086efb932b0
László Varga <108331825+Laszlo-Varga@users.noreply.github.com>
2023-10-04 13:45:28 +0200

    Implement Customer,NewCustomerRequest and CustomerService classes.

--- backend/src/main/java/com/ThreeTree/service/CustomerService.java
+++ backend/src/main/java/com/ThreeTree/service/CustomerService.java
@@ -1,4 +1,32 @@
 package com.ThreeTree.service;
 
+import com.ThreeTree.dao.CustomerRepository;
+import com.ThreeTree.model.Customer;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CustomerService {
+    private final CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
+
+    public Customer getCustomerById(Integer id) {
+        return customerRepository.findById(id).orElseThrow();
+    }
+
+    public void addCustomer(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
 }
