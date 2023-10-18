fa512b195449694cc9afd47fd4e8569e8f934231
László Varga <108331825+Laszlo-Varga@users.noreply.github.com>
2023-10-18 14:53:29 +0200

    Change background

--- frontend/ThreeTree/src/App.jsx
+++ frontend/ThreeTree/src/App.jsx
@@ -14,12 +14,9 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home/>}/>
                     <Route path="/signup" element={<SignUp/>}/>
-<<<<<<< HEAD
                     <Route path="/products" element={<Products/>}/>
-=======
                     <Route path="/successfullySignedUp" element={<SuccessfullySignedUp/>}/>
                     <Route path="/successfullyOrdered" element={<SuccessfullyOrdered/>}/>
->>>>>>> a8abd4b3689f104fe17b2b359ea0a0e8aff5805e
                 </Routes>
             </div>
         </Router>
--- frontend/ThreeTree/src/component/SuccessfullyOrdered.jsx
+++ frontend/ThreeTree/src/component/SuccessfullyOrdered.jsx
@@ -7,7 +7,7 @@ function SuccessfullyOrdered() {
     const theme = useTheme();
 
     const customStyles = {
-        backgroundImage: 'url("https://img.freepik.com/premium-photo/tropical-palm-leaves-jungle-leaves-seamless-vector-floral-pattern-background_354831-3425.jpg")',
+        backgroundImage: 'url("https://images.unsplash.com/photo-1497250681960-ef046c08a56e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80")',
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
