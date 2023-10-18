5750a8fdafacb1fe95888e9ab4b65e16a3e60b8a
Nagy Lőrinc <nagy.lorinc7@gmail.com>
2023-10-18 11:02:51 +0200

    Create SuccessFullySignedUp component

--- frontend/ThreeTree/src/App.jsx
+++ frontend/ThreeTree/src/App.jsx
@@ -1,6 +1,8 @@
 import SignUp from './component/SignUp.jsx';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './component/Home.jsx';
+import SuccessfullySignedUp from "./component/SuccessFullySignedUp.jsx";
+
 
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home/>}/>
                     <Route path="/signup" element={<SignUp/>}/>
+                    <Route path="/successfullySignedUp" element={<SuccessfullySignedUp/>}/>
                 </Routes>
             </div>
         </Router>
