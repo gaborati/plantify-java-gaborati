2bf10a6df4684d1423efe7a3f5a0219c79f8c9ef
László Varga <108331825+Laszlo-Varga@users.noreply.github.com>
2023-10-19 13:07:30 +0200

    Change annotations in model layer and change endpoint at OrderController

--- backend/src/main/java/com/ThreeTree/model/Product.java
+++ backend/src/main/java/com/ThreeTree/model/Product.java
@@ -1,7 +1,6 @@
 package com.ThreeTree.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
 
