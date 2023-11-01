f9104b9b3dc1f7a4b28fe727b9e1d0d76f1042e0
Nagy Lőrinc <nagy.lorinc7@gmail.com>
2023-11-01 10:40:55 +0100

    Implement UserDetails in Person entity

--- backend/src/main/java/com/ThreeTree/dao/PersonRepository.java
+++ backend/src/main/java/com/ThreeTree/dao/PersonRepository.java
@@ -3,5 +3,9 @@ package com.ThreeTree.dao;
 import com.ThreeTree.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    Optional<Person> findByEmail(String email);
 }
