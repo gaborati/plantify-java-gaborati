33d58c0b489b4c993c6c6afd3b80c367943fb156
László Varga <108331825+Laszlo-Varga@users.noreply.github.com>
2023-11-17 14:54:52 +0100

    Debug vite.config.js file

--- frontend/ThreeTree/vite.config.js
+++ frontend/ThreeTree/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 export default defineConfig({
     plugins: [react()],
+    server: {
+        proxy: {
+            '/api': 'http://localhost:8080'
+        }
+    },
     build: {
         rollupOptions: {
             input: 'src/main.jsx', // Specify the correct entry point
